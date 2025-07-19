1059b6162bdd16bdebc68bf7f08064e33029ec57
Nafeesa Kardigudda <nafeesakardigudda786@gmail.com>
2025-07-19 16:52:20 +0530

    cors and handling 404 error and swaager for api documentaion

--- server.js
+++ server.js
@@ -1,29 +1,63 @@
 //1. Import Express
-import express from 'express'
+import express from 'express';
+import swagger from 'swagger-ui-express';
+import cors from 'cors';
 import productRouter from './src/features/product/product.routes.js';
 import userRouter from './src/features/user/user.routes.js';
 import cartRouter from './src/features/cart/cart.routes.js';
 import bodyParser from 'body-parser'
 //import basicAuthorizer from './src/middleware/basicAuth.middleware.js';
 import jwtAuth from './src/middleware/jwt.middleware.js';
-//Create Server
-const server=express();
 
+import apiDocs from './swagger.json' with { type: 'json' };
+//2. Create Server
+const server=express();
+//CORS policy configuartion
+//using package cors
+
+var corsOptions = {
+  origin: 'http://127.0.0.1:5500',
+  allowedHeaders: '*' // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+server.use(cors(corsOptions))
+//using cors res header
+//'http://localhost:5500' to '*' for public access
+// server.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500'); // Match the actual origin
+//     res.header('Access-Control-Allow-Headers', '*'); // Consider specifying exact headers
+//     res.header('Access-Control-Allow-Methods', '*'); // Consider specifying exact methods
+
+//     // Return OK for preflight request
+//     if (req.method === "OPTIONS") {
+//         res.sendStatus(200);
+//     } else {
+//         next();
+//     }
+// });
 //parsing data into json format
 server.use(bodyParser.json())
 //for all the request related to Product, redirt to product routes
 //localhost:3200/api/products
+
+// Bearer <token>
+server.use("/api-docs", 
+swagger.serve, 
+swagger.setup(apiDocs))
 server.use('/api/products',jwtAuth,productRouter)
 server.use('/api/users',userRouter)
 
 server.use('/api/cart',jwtAuth,cartRouter)
 
 
-//default request handler
+//3.default request handler
 server.get('/',(req,res)=>{
     res.send("Welcome to E-commerce API")
 })
-
+//4. Middleware to handle 404 request
+//5. Server listeninng port
+server.use((req,res)=>{
+    res.status(404).send("API not found. Please check our documentation for more information at /api-docs")
+})
 const PORT=3200;
 
 server.listen(PORT,(req,res)=>{
